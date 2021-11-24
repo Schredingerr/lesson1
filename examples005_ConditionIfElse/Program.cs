80aefe485331d69f3cd8f5b1c46b483d19702e35
Schredinger <gameboxofschredinger@gmail.com>
2021-11-24 19:22:51 +0300

    add example 005 Condition If Else

---
+++ examples005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("ВВедите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "schredinger")
+{
+    Console.WriteLine("Schredinger, а мы твоего кота съели =)");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
